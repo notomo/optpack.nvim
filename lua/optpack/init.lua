1d062d3575dd11567898cf6def1a9f2208ca1dee
notomo <notomo.motono@gmail.com>
2023-02-24 21:48:38 +0900

    Add type doc

--- lua/optpack/init.lua
+++ lua/optpack/init.lua
@@ -1,33 +1,94 @@
 local M = {}
 
+--- @class OptpackAddOption
+--- @field depends string[]? dependency plugin (not full) names. These will be loaded before. default: {}
+--- @field enabled boolean? if enabled=false, optpack ignores the plugin. default: true
+--- @field package_name string? used as the package directory name. your-packpath/pack/{package_name}/opt default: "optpack"
+--- @field select_packpath (fun():string)? returns a directory path. default: returns the first element of 'packpath'
+--- @field fetch OptpackAddOptionFetch? |OptpackAddOptionFetch|
+--- @field hooks OptpackAddOptionHooks? |OptpackAddOptionHooks|
+--- @field load_on OptpackAddOptionLoadOn? |OptpackAddOptionLoadOn|
+
+--- @class OptpackAddOptionFetch
+--- @field base_url string? A git server base url, default: "https://github.com"
+--- @field depth integer? for shallow clone depth. Used full clone if depth < 1. default: 1
+
+--- @class OptpackAddOptionHooks
+--- @field post_add fun(plugin:OptpackPlugin)? called on after |optpack.add()|
+--- @field pre_load fun(plugin:OptpackPlugin)? called on before loading
+--- @field post_install fun(plugin:OptpackPlugin)? called on after installing
+--- @field post_update fun(plugin:OptpackPlugin)? called on after updating
+--- @field post_load fun(plugin:OptpackPlugin)? called on after loading
+
+--- @class OptpackAddOptionLoadOn
+--- @field cmds string[]? EX command pattern list, default: {}
+--- @field events (string|string[])[]? autocmd event name or [name, pattern] list, default: {}
+--- @field filetypes string[]? file type pattern list, default: {}
+--- @field keymaps fun(vim:table)? set keymap function. (The argument is patched vim object)
+---   example: function(vim) vim.keymap.set("n", "S", "<Plug>(example)") end
+---   default: function() end
+--- @field modules string[]? for require() lua module name list, default: {}
+
 --- Add a plugin.
 --- @param full_name string: {account_name}/{plugin_name} format
---- @param opts table|nil: |optpack.nvim-add-option|
+--- @param opts OptpackAddOption?: |OptpackAddOption|
 function M.add(full_name, opts)
   require("optpack.command").add(full_name, opts)
 end
 
+--- @class OptpackPlugin
+--- @field directory string full path of the plugin directory
+--- @field full_name string {account_name}/{plugin_name} format plugin name
+--- @field name string plugin name
+--- @field url string git repository url
+--- @field opts OptpackAddOption |OptpackAddOption|
+
 --- Returns list of plugins.
---- @return table: list of |optpack.nvim-plugin|
+--- @return OptpackPlugin[]: list of |OptpackPlugin|
 function M.list()
   return require("optpack.command").list()
 end
 
 --- Returns a plugin.
 --- @param plugin_name string:
---- @return table: |optpack.nvim-plugin|
+--- @return OptpackPlugin: |OptpackPlugin|
 function M.get(plugin_name)
   return require("optpack.command").get(plugin_name)
 end
 
+--- @class OptpackInstallOrUpdateOption
+--- @field on_finished fun(plugin:OptpackPlugin)? called on finished updating or installing
+--- @field outputters OptpackOutputters? outputter settings
+--- @field parallel OptpackParallelOption? parallel setting
+--- @field pattern string? target plugin name pattern for vim regex, default: ".*"
+
+--- @class OptpackOutputters
+--- @field buffer OptpackBufferOutputter? |OptpackBufferOutputter|
+--- @field echo OptpackEchoOutputter? |OptpackEchoOutputter|
+--- @field log OptpackLogOutputter? |OptpackLogOutputter|
+
+--- @class OptpackBufferOutputter
+--- @field enabled boolean? used if true. default: true
+--- @field open fun(bufnr:integer)? open buffer
+
+--- @class OptpackEchoOutputter
+--- @field enabled boolean? used if true. default: true
+
+--- @class OptpackLogOutputter
+--- @field enabled boolean? used if true. default: true
+--- @field path string? log file path: default: vim.fn.stdpath("cache")/optpack-update.log
+
+--- @class OptpackParallelOption
+--- @field limit integer? max number for parallel job, default: 8
+
 --- Install plugins.
---- @param opts table|nil: |optpack.nvim-install-or-update-option|
+--- @param opts OptpackInstallOrUpdateOption?: |OptpackInstallOrUpdateOption|
 function M.install(opts)
   require("optpack.command").install_or_update("install", opts)
 end
 
 --- Update plugins.
---- @param opts table|nil: |optpack.nvim-install-or-update-option|
+--- @param opts OptpackInstallOrUpdateOption?: |OptpackInstallOrUpdateOption|
 function M.update(opts)
   require("optpack.command").install_or_update("update", opts)
 end
@@ -44,8 +105,12 @@ function M.load_by_expr_keymap(...)
   return ""
 end
 
+--- @class OptpackSetting
+--- @field add OptpackAddOption? |OptpackAddOption|
+--- @field install_or_update OptpackInstallOrUpdateOption? |OptpackInstallOrUpdateOption|
+
 --- Set default setting.
---- @param setting table: |optpack.nvim-setting|
+--- @param setting OptpackSetting: |OptpackSetting|
 function M.set_default(setting)
   require("optpack.command").set_default(setting)
 end
--- spec/lua/optpack/doc.lua
+++ spec/lua/optpack/doc.lua
@@ -15,152 +15,19 @@ require("genvdoc").generate(full_plugin_name, {
         return "Lua module: " .. group
       end,
       group = function(node)
-        if not node.declaration then
+        if node.declaration == nil or node.declaration.type ~= "function" then
           return nil
         end
         return node.declaration.module
       end,
     },
     {
-      name = "TYPES",
-      body = function(ctx)
-        local add_option_text
-        do
-          local descriptions = {
-            enabled = [[(boolean | nil): if enabled=false, optpack ignores the plugin.
-    default: %s]],
-            depends = [[(table | nil): dependency plugin (not full) names.
-    These will be loaded before.
-    default: {}]],
-            fetch = {
-              text = [[(table | nil): fetch setting]],
-              children = {
-                depth = [[for shallow clone depth. Used full clone if depth < 1.
-    default: %s]],
-                base_url = [[A git server base url,
-    default: %s]],
-              },
-            },
-            hooks = {
-              text = [[(table | nil): hook functions for some timings]],
-              children = {
-                post_add = [[(function | nil) called on after |optpack.add()|]],
-                pre_load = [[(function | nil) called on before loading]],
-                post_load = [[(function | nil) called on after loading]],
-                post_install = [[(function | nil) called on after installing]],
-                post_update = [[(function | nil) called on after updating]],
-              },
-            },
-            load_on = {
-              text = [[(table | nil): setting for lazy loading]],
-              children = {
-                events = [[(table | nil): autocmd event name or [name, pattern] list,
-    default: %s]],
-                modules = [[(table | nil): for require() lua module name list,
-    default: %s]],
-                cmds = [[(table | nil): EX command pattern list,
-    default: %s]],
-                filetypes = [[(table | nil): file type pattern list,
-    default: %s]],
-                keymaps = [[(function | nil): set keymap function. (The argument is patched vim object)
-    example: function(vim) vim.keymap.set("n", "S", "<Plug>(example)") end
-    default: function() end]],
-              },
-            },
-            package_name = [[(string | nil): used as the package directory name.
-    your-packpath/pack/{package_name}/opt
-    default: %s]],
-            select_packpath = [[(function | nil): returns a directory path.
-    default: returns the first element of 'packpath']],
-          }
-          local default = require("optpack.core.option").AddOption.default
-          local keys = vim.tbl_keys(default)
-          local lines = util.each_keys_description(keys, descriptions, default)
-          add_option_text = table.concat(lines, "\n")
+      name = "STRUCTURE",
+      group = function(node)
+        if node.declaration == nil or node.declaration.type ~= "class" then
+          return nil
         end
-
-        local install_or_update_option_text
-        do
-          local descriptions = {
-            on_finished = [[(function | nil): called on finished updating or installing]],
-            outputters = {
-              text = [[(table | nil): outputter settings.]],
-              children = {
-                buffer = {
-                  text = [[(table): buffer output setting]],
-                  children = {
-                    open = [[(function | nil) (bufnr) -> open buffer]],
-                    enabled = [[(boolean): used if true. default: %s]],
-                  },
-                },
-                echo = {
-                  text = [[(table): echo output setting]],
-                  children = {
-                    enabled = [[(boolean): used if true. default: %s]],
-                  },
-                },
-                log = {
-                  text = [[(table): log output setting]],
-                  children = {
-                    path = [[(string): log file path: default: vim.fn.stdpath("cache")/optpack-update.log]],
-                    enabled = [[(boolean): used if true. default: %s]],
-                  },
-                },
-              },
-            },
-            parallel = {
-              text = [[(table | nil): parallel setting]],
-              children = { limit = [[max number for parallel job,  default: %s]] },
-            },
-            pattern = [[(string | nil): target plugin name pattern for vim regex,
-    default: %s]],
-          }
-          local default = require("optpack.core.option").InstallOrUpdateOption.default
-          local keys = vim.tbl_keys(default)
-          local lines = util.each_keys_description(keys, descriptions, default)
-          install_or_update_option_text = table.concat(lines, "\n")
-        end
-
-        local plugin_text
-        do
-          local descriptions = {
-            directory = [[(string): full path of the plugin directory]],
-            full_name = [[(string): {account_name}/{plugin_name} format plugin name]],
-            name = [[(string): plugin name]],
-            url = [[(string): git repository url]],
-            opts = [[(table): |optpack.nvim-add-option|]],
-          }
-          local default =
-            require("optpack.core.plugin").Plugin.new("doc", require("optpack.core.option").AddOption.new()):expose()
-          local keys = vim.tbl_keys(default)
-          local lines = util.each_keys_description(keys, descriptions)
-          plugin_text = table.concat(lines, "\n")
-        end
-
-        local setting_text
-        do
-          local descriptions = {
-            add = [[(table | nil): |optpack.nvim-add-option|
-    default: %s]],
-            install_or_update = [[(table | nil): |optpack.nvim-install-or-update-option|
-    default: %s]],
-          }
-          local default = require("optpack.core.option").default
-          local keys = vim.tbl_keys(default)
-          local lines = util.each_keys_description(keys, descriptions, default)
-          setting_text = table.concat(lines, "\n")
-        end
-
-        return util.sections(ctx, {
-          { name = "|optpack.add()| option", tag_name = "add-option", text = add_option_text },
-          {
-            name = "|optpack.install()| or |optpack.update()| option",
-            tag_name = "install-or-update-option",
-            text = install_or_update_option_text,
-          },
-          { name = "Plugin", tag_name = "plugin", text = plugin_text },
-          { name = "|optpack.set_default()| setting", tag_name = "setting", text = setting_text },
-        })
+        return "STRUCTURE"
       end,
     },
     {
