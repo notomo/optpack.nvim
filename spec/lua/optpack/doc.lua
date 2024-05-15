fef635adcbd7bc076f6ec41b103ec0e672486707
notomo <notomo.motono@gmail.com>
2024-05-15 21:31:01 +0900

    Small refactor doc gen

--- spec/lua/optpack/doc.lua
+++ spec/lua/optpack/doc.lua
@@ -39,9 +39,7 @@ require("genvdoc").generate(full_plugin_name, {
 })
 
 local gen_readme = function()
-  local f = io.open(example_path, "r")
-  local exmaple = f:read("*a")
-  f:close()
+  local exmaple = util.read_all(example_path)
 
   local content = ([[
 # %s
@@ -53,8 +51,6 @@ This is a neovim plugin manager that uses only opt package.
 ```lua
 %s```]]):format(full_plugin_name, exmaple)
 
-  local readme = io.open("README.md", "w")
-  readme:write(content)
-  readme:close()
+  util.write("README.md", content)
 end
 gen_readme()
